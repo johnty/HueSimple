29fed8a6225c5d8330a32ec9b6332513376218c7
Johnty Wang <johntywang@gmail.com>
2016-01-13 22:10:35 -0500

    added RtMidi headers directly, working with ICubeX interface running in standalone mode with single sensor sending ctrl messages (0-127)

--- HueSimple.pro
+++ HueSimple.pro
@@ -7,5 +7,12 @@ CONFIG -= app_bundle
 
 TEMPLATE = app
 
-SOURCES += main.cpp
+win32:LIBS += winmm.lib
+win32:DEFINES += __WINDOWS_MM__
+
+SOURCES += main.cpp \
+           RtMidi.cpp
+
+#for stk root
+INCLUDEPATH += ../../include/ ../../src/include/
 
--- main.cpp
+++ main.cpp
@@ -6,17 +6,36 @@
 #include <QJsonObject>
 #include <QJsonDocument>
 #include <QTimer>
-#include <QCommandLineParser>
+#include "RtMidi.h"
 
+//put IP address and API key for Hue bridge here:
+const QString api_root = "http://192.168.100.230/api/b225912329de4371bdc4d2e18678263/lights/";
+
+//put light index and number of lights here:
+const int lights[] = {4, 12};
+const int num_lights = 2;
 
 void setLightOn(bool isOn, int idx, QNetworkAccessManager* man)
 {
-    QString url_str = "http://192.168.100.230/api/b225912329de4371bdc4d2e18678263/lights/" + QString::number(idx) + "/state/";
+    QString url_str = api_root + QString::number(idx) + "/state/";
     QUrl url(url_str);
     QNetworkRequest request(url);
     request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
     QJsonObject data_obj;
     data_obj.insert("on", isOn);
+    QJsonDocument doc(data_obj);
+    QByteArray data = doc.toJson();
+    man->put(request, data);
+}
+
+void setBrightness(int bri, int idx, QNetworkAccessManager* man)
+{
+    QString url_str = api_root + QString::number(idx) + "/state/";
+    QUrl url(url_str);
+    QNetworkRequest request(url);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+    QJsonObject data_obj;
+    data_obj.insert("bri", bri);
     QJsonDocument doc(data_obj);
     QByteArray data = doc.toJson();
     man->put(request, data);
@@ -28,54 +47,77 @@ int main(int argc, char *argv[])
 
     QNetworkAccessManager* manager = new QNetworkAccessManager();
 
-    QTimer::singleShot(2000, &a, SLOT(quit())); //trigger stop
+    //set up midi
 
+    RtMidiIn* midiIn = new RtMidiIn();
+    midiIn->openPort(0);
+    qDebug() <<"num ports = " << midiIn->getPortCount();
+    qDebug() <<"opened Midi port " << midiIn->getPortName(0).c_str();
 
-
-    QCommandLineParser parser;
-    parser.setApplicationDescription("Hue Simple Controller");
-    parser.addHelpOption();
-    parser.addVersionOption();
-
-    //QCommandLineOption onOption("on", QCoreApplication::translate("main", "turn light(s) on"));
-    //parser.addOption(onOption);
-
-    parser.addPositionalArgument("index", "index(s) of light(s)");
-
-    parser.process(a);
-
-
-    const QStringList options = parser.positionalArguments();
-    if (options.length() >= 2)
+    //NOTE:we don't do any checking so make sure
+    // the variables at top are set!!
+    for (int i=0; i<num_lights; ++i)
     {
-        qDebug()<< "isOn = " << options.at(0);
+        setLightOn(true, lights[i], manager);
+        setBrightness(255, lights[i], manager);
+    }
+    std::vector<uchar> message;
 
-        bool isOn;
-        if (options.at(0) == "on")
-            isOn = true;
-        else
-            isOn = false;
+    uint bri0 = 0;
+    uint bri1 = 0;
+    uint bri = 0;
+    uint pre_bri = 0;
+    bool isOn = true;
+    while (1)
+    {
+        //do midi object poll
+        double tstamp = midiIn->getMessage(&message);
 
-        for (int i=1; i<options.length(); ++i)
+        if (message.size() == 3)
         {
+            qDebug() << message.at(0) <<" "
+                        << message.at(1) <<" "
+                           << message.at(2);
 
-            QString idx_str = options.at(i);
-            int idx = idx_str.toInt();
-            qDebug()<< "idx = " << idx;
-            setLightOn(isOn, idx, manager);
+            bri0 = message.at(2);
+            bri1 = bri0;
+            bri = bri0 + bri1; //kind of filter here...
         }
+        //int bri = message.at(2);
+        if (bri != pre_bri)
+        {
+            for (int i=0; i<num_lights; ++i)
+            {
+                setBrightness(bri, lights[i], manager);
+            }
+            pre_bri = bri;
 
+            if (bri < 10)
+            {
+                for (int i=0; i<num_lights; ++i)
+                {
+                    setLightOn(false, lights[i], manager);
+                }
+                isOn = false;
+                qDebug()<<"OFF!";
+            }
+            else
+            {
+                if (!isOn)
+                {
+                    for (int i=0; i<num_lights; ++i)
+                    {
+                        setLightOn(true, lights[i], manager);
+                    }
+                    qDebug()<<"ON!";
+                }
+            }
+        }
+        //process message loop
+        QCoreApplication::processEvents();
     }
 
-    //setLightOn(isOn, i, manager);
-
-
-
-
-        //setLightOn(isOn, index, manager);
-
-    //return 0;
-
-    return a.exec();
+    //cleanup
+    delete midiIn;
 }
 
