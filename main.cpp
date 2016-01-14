965f0e8d5bd52c8ef7427c64c91cb23158c74b04
Johnty Wang <johntywang@gmail.com>
2016-01-14 08:43:07 -0500

    added missing logic to prevent on messages being sent all the time (which slow down hue response!)

--- main.cpp
+++ main.cpp
@@ -111,6 +111,7 @@ int main(int argc, char *argv[])
                     {
                         setLightOn(true, lights[i], manager);
                     }
+                    isOn = true;
                     qDebug()<<"ON!";
                 }
             }
